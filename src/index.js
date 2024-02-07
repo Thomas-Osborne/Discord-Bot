18bb98c2fc327512b6299a7818469e2136717e58
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-02-07 12:53:19 +0000

    Put bot online

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+require('dotenv').config();
+
 const { Client, IntentsBitField } = require('discord.js');
 
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.login(process.env.TOKEN);
+console.log("The bot is online.")
