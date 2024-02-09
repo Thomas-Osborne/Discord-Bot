73fe76e4324974ce6f86187a24cd0f888f544287
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-02-09 15:59:44 +0000

    Added random duel command

--- src/commands/hearthstone/card.js
+++ src/commands/hearthstone/card.js
@@ -26,15 +26,20 @@ module.exports = {
 
           const card = await hsClient.card({ id: cardName });
 
+          console.log(card.data);
+
+          const cardDescription = card.data.text.replace(/<\/?[^>]+(>|$)/g, ""); // regex to strip HTML tags
+
           const embed = new EmbedBuilder()
           .setTitle(card.data.name)
-          .setDescription(`${card.data.text}\n\n_${card.data.flavorText}_`)
+          .setDescription(`${cardDescription}\n\n_${card.data.flavorText}_`)
           .addFields(
                 {name: 'Mana', value: `${card.data.manaCost}`},
                 {name: 'Attack', value: `${card.data.attack}`, inline: true},
                 {name: 'Health', value: `${card.data.health}`, inline: true}
           )
           .setTimestamp(Date.now())
+          .setThumbnail(card.data.cropImage)
           .setImage(card.data.image);
 
           await interaction.reply({ embeds: [embed]});
---
+++ src/commands/misc/duel.js
@@ -0,0 +1,33 @@
+const { ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+    name: 'duel',
+    description: 'Duel a member from the server.',
+    devOnly: true,
+    options: [
+        {
+            name: 'target-user',
+            description: 'The user to duel.',
+            required: true,
+            type: ApplicationCommandOptionType.Mentionable,
+        },
+        {
+            name: 'amount',
+            description: 'Amount to wager.',
+            required: true,
+            type: ApplicationCommandOptionType.Number,
+        },
+    ],
+
+    callback: (client, interaction) => {
+
+        const amount = interaction.options.get('amount').value;
+        const target = interaction.options.get('target-user').member; // useful to have the object as a guild member
+
+        if (Math.random() < 0.5) {
+            interaction.reply(`<@${interaction.member.id}> you win against <@${target.id}>! You double your wager to £${2 * amount}.`);
+        } else {
+            interaction.reply(`<@${interaction.member.id}> you win against <@${target.id}>! You lose your £${amount}.`);
+        }
+    }
+}
