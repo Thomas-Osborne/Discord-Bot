3870982d2569b3556e471a08df95600fe5bd78c4
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-02-11 20:57:26 +0000

    Corrected calculations

--- src/commands/solar/forecast.js
+++ src/commands/solar/forecast.js
@@ -9,10 +9,14 @@ module.exports = {
     callback: (client, interaction) => {
         const NUMBER_OF_FORECASTS = 48;
         const TOTAL_BATTERY_CAPACITY = 24.3;
-        const MAX_CHARGING_TIME = 360;
+        const MAX_CHARGING_HOURS = 6;
         const AV_BATTERY_VOLTAGE = 52;
+        const MAX_CURRENT = 70;
+        const DAILY_USAGE = 18;
+
         let desiredIncrease;
         let earliestHour;
+        let chargingHours;
 
         forecasts = forecastsData.forecasts;
         let times = []
@@ -33,13 +37,13 @@ module.exports = {
             times.push(forecasts[i].period_end.getTime());
         }
 
-        console.log("HERE", forecasts[0].period_end.getUTCHours());
-
         if (forecasts[0].period_end.getUTCHours() <= 5) {
             console.log("Time between 00:00 and 05:00: show today's data.");
+            chargingHours = MAX_CHARGING_HOURS - forecasts[0].period_end.getUTCHours() - 1;
             index = 0;
         } else { 
             console.log("Time after 05:00: show tomorrow's data.");
+            chargingHours = MAX_CHARGING_HOURS;
             for (let i = 0; i < forecasts.length; i++) {
                 if (forecasts[i].period_end.getUTCHours() == 0) {
                     index = i;
@@ -48,18 +52,22 @@ module.exports = {
             }
         }
 
+        chargingHours = 4;
+
         let message = '';
         for (let i = index; i < index + NUMBER_OF_FORECASTS; i++) {
             message += `${formatTime(times[i])}\n`;
 
         }
 
-        desiredIncrease = 50;
+        desiredIncrease = 32;
 
-        let kwPerHour = (TOTAL_BATTERY_CAPACITY * (desiredIncrease / 100)) / (MAX_CHARGING_TIME / 60);
-        let current = kwPerHour * 1000 / AV_BATTERY_VOLTAGE;
+        let kwPerHoursRequired = (TOTAL_BATTERY_CAPACITY) * (desiredIncrease) / 100;
+        let kwPerHourRate = (kwPerHoursRequired) / Math.min(chargingHours, MAX_CHARGING_HOURS);
+        let current = Math.min(kwPerHourRate * 1000 / AV_BATTERY_VOLTAGE, MAX_CURRENT);
 
-        interaction.reply(message);
+        console.log(kwPerHoursRequired);
+        interaction.reply(`current: ${Math.round(current * 100) / 100}`); // round to 2dp
     
     
     }
