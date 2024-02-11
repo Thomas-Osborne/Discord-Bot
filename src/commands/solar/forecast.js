490df040ceecd993da9fc0473683571e2ee12ae3
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-02-11 21:45:34 +0000

    Updated output

--- src/commands/solar/forecast.js
+++ src/commands/solar/forecast.js
@@ -22,15 +22,6 @@ module.exports = {
         let index;
 
         currentTime = new Date();
-        // currentTime = new Date(2024, 1, 11, 2, 1, 1, 0);
-        console.log(currentTime);
-        console.log(currentTime.getHours());
-        if (currentTime.getHours() <= 5) {
-            console.log("Time before 5")
-        } else {
-            console.log("Time after 5");
-        }
-
 
         let analysedData = [];
         
@@ -43,7 +34,8 @@ module.exports = {
         const today = forecasts[0].period_end;
 
         const todaysDate = forecasts[0].period_end.getUTCDate();
-        const tomorrowsDate = today.getDate() + 1;
+        const tomorrowsDate = today.getDate() + 1; // DOES NOT WORK FOR END OF MONTHS
+    
 
         if (currentTime.getHours() <= 5) {
             console.log("Time between 00:00 and 05:00: show today's data.");
@@ -58,6 +50,7 @@ module.exports = {
             }
             index = 0;
         } else { 
+            
             console.log("Time after 05:00: show tomorrow's data.");
             chargingHours = MAX_CHARGING_HOURS;
             for (let i = 0; i < forecasts.length; i++) {
@@ -71,9 +64,9 @@ module.exports = {
             }
         }
 
-        console.log(chargingHours);
+        const expectedYield = estimatePv(analysedData);
 
-        console.log(analysedData);
+        console.log(expectedYield);
 
         let message = '';
         for (let i = index; i < index + NUMBER_OF_FORECASTS; i++) {
@@ -81,15 +74,42 @@ module.exports = {
 
         }
 
-        desiredIncrease = 32;
+        desiredIncrease = 49;
+
+        for (let i = 0; i < analysedData.length; i++) {
+            console.log(analysedData[i].pv_estimate);
+        }
 
         let kwPerHoursRequired = (TOTAL_BATTERY_CAPACITY) * (desiredIncrease) / 100;
         let kwPerHourRate = (kwPerHoursRequired) / Math.min(chargingHours, MAX_CHARGING_HOURS);
         let current = Math.min(kwPerHourRate * 1000 / AV_BATTERY_VOLTAGE, MAX_CURRENT);
 
-        console.log(kwPerHoursRequired);
-        interaction.reply(`current: ${Math.round(current * 100) / 100}`); // round to 2dp
+        interaction.reply(
+        `charging hours: ${chargingHours}
+        desired increase: ${desiredIncrease}
+        required kwh: ${Math.round(kwPerHoursRequired * 100)/ 100}
+        current: ${Math.round(current * 100) / 100}`); // round to 2dp
     
     
     }
 }
+
+function estimatePv(data) {
+    // use trapezium rule
+    const TIME_GRANULARITY = 0.5;
+    let estimate = 0;
+    if (data.length == 0) {
+        return 0;
+    } else if (data.length == 1) {
+        return data[0].pv_estimate;
+    } else {
+        estimate += data[0].pv_estimate;
+        for (let i = 1; i < data.length - 1; i++) {
+            estimate += 2 * data[i].pv_estimate;
+        }
+        estimate += data[data.length - 1].pv_estimate;
+        estimate = TIME_GRANULARITY / 2 * estimate;
+        return estimate;
+    }
+}
+
